110cc168eb990482db9f7e51cb90e78e139d7f6e
leeredden <leeredden@gmail.com>
2016-11-27 14:17:51 -0800

    removed ipython code and extra comments in webcam class

--- capture/webcam.py
+++ capture/webcam.py
@@ -1,34 +1,3 @@
-# import numpy as np
-# import cv2
-# import time
-
-# cap = cv2.VideoCapture(0)
-
-# count = 100
-
-# while(count):
-#     # Capture frame-by-frame
-#     ret, frame = cap.read()
-
-#     # Our operations on the frame come here
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-#     # Display the resulting frame
-#     try:
-#         cv2.imshow('frame',gray)
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
-#     except: 
-#         print "frame wasn't captures correctly"
-        
-#     count -= 1
-#     time.sleep(0.1)
-    
-    
-# cap.release()
-# #out.release()
-# cv2.destroyAllWindows()
-
 import numpy as np
 import cv2
 import time
